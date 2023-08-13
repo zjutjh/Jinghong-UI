404e4c9c3dc1826f615f676555e2a8076b4d239e
Finley Ge <m13203533462@163.com>
2023-08-13 23:37:19 +0200

    feat: JLayout

---
+++ packages/components/src/JLayout/JLayout.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { inject, ref } from 'vue';
+import { ColorThemeKey } from '../provide';
+import { JColorThemeDefault } from '../colors';
+
+const color = inject(ColorThemeKey, ref(JColorThemeDefault))
+
+</script>
+<template>
+  <div class="j-layout">
+    <header>
+      <slot name="header" />
+    </header>
+    <main>
+      <slot name="main" />
+    </main>
+    <footer>
+      <slot name="footer" />
+    </footer>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.j-layout {
+  display: flex;
+  flex-direction: column;
+  color: v-bind('color.text');
+
+  header {
+    background-color: v-bind('color.backgroundSecondary');
+    padding-inline: 10px;
+    padding-block: 5px;
+  }
+
+  main {
+    background-color: v-bind('color.background');
+    padding-inline: 10px;
+    padding-block: 5px;
+  }
+
+  footer {
+    background-color: v-bind('color.backgroundSecondary');
+    padding-inline: 10px;
+    padding-block: 5px;
+  }
+
+}
+</style>
---
+++ packages/components/src/JLayout/type.ts
@@ -0,0 +1,3 @@
+import { ExtractPropTypes } from 'vue';
+import JLayout from './JLayout';
+export type JLayoutProps = ExtractPropTypes<typeof JLayout>;
